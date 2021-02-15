84f5e81750343f5bd8d672a6798b7e0e026a39b1
Ana <ana.gamez204360@potros.itson.edu.mx>
2021-02-14 21:33:26 -0700

    colores

--- app/src/main/java/gamez/ana/asignacion04_calculadora_imc/MainActivity.kt
+++ app/src/main/java/gamez/ana/asignacion04_calculadora_imc/MainActivity.kt
@@ -2,10 +2,30 @@ package gamez.ana.asignacion04_calculadora_imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var valorIMC = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val peso : EditText = findViewById(R.id.peso)
+        val estatura : EditText = findViewById(R.id.estatura)
+        val btnCalcular : Button = findViewById(R.id.btnCalcular)
+        val imc : TextView = findViewById(R.id.imc)
+        val range : TextView = findViewById(R.id.range)
+
+        //evento de botones
+
+        btnCalcular.setOnClickListener {
+            
+        }
     }
+
+    //funciones
 }
