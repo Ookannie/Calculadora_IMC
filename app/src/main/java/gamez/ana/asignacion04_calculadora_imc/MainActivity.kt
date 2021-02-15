82ee0327aaeeaece3c645abb6e556e40c344bf88
Ana <ana.gamez204360@potros.itson.edu.mx>
2021-02-14 19:47:12 -0700

    first commit

---
+++ app/src/main/java/gamez/ana/asignacion04_calculadora_imc/MainActivity.kt
@@ -0,0 +1,11 @@
+package gamez.ana.asignacion04_calculadora_imc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
